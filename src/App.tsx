77288d1022b86d200b0df9cee50cae3677098c25
findsakshat <voteforsakshat@gmail.com>
2024-04-02 18:21:04 +0530

    misc: small fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
+import './App.css';
 import CustomHookPatternExample from './04-custom-hooks-pattern/index';
 import CompoundComponentsPatternExample from './03-compound-components-pattern';
-import './App.css'
 import ContainerPresentationalPatternExample from './02-container-presentational-components-pattern';
 import ContextAPIPatternExample from './05-context-api-pattern';
 import ControlledAndUnControlledComponentPatternExample from './06-controlled-and-uncontrolled-components-pattern';
